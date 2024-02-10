f6558f386b515377afbb405de4b3d68fb8407311
James Anderson <james.r.anderson@utah.edu>
2024-02-09 16:37:07 -0800

    More fixes

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,7 @@
 """This package contains read-only record objects used for configuration"""
 
 from .range_config import Range, OptionalRange
-from .pid import PIDConfig
+from .pid import PIDConfig, FeedForwardConfig
 
 from . import physical_config
 from .physical_config import PhysicalConfig
@@ -13,7 +13,17 @@ from .swerve_module_config import SwerveModuleConfig, MotorConfig, \
                                   OptionalSwerveModuleFloatProperty, OptionalSwerveModuleIntProperty
 
 from . import driver_controls
-from .driver_controls import DriverControlsConfig
+from .driver_controls import DriverControlsConfig, AxisConfig, \
+                             ControllerConfig, ControllerKey, \
+                             ControllerType
 
-from . import driver_controls
-from .driver_controls import AxisConfig
+from . import climber
+from .climber import ClimberConfig
+
+from . import shooter
+from .shooter import ShooterConfig
+
+from . import indexer
+from .indexer import IndexerConfig
+
+
