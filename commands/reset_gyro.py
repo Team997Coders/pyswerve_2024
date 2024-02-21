b18d4305fe7c9e58dad2030acfdc9db2d060398f
Alex_He <118498365+Yuxuan1103@users.noreply.github.com>
2024-02-20 20:40:42 -0800

    reset gyro done

---
+++ commands/reset_gyro.py
@@ -0,0 +1,16 @@
+
+import navx
+import commands2
+class reset_gyro(commands2.InstantCommand):
+
+    def __init__(self):
+        # Calls the constructor of our parent class if it exists.  Do this first when inheriting from a parent class.
+        # This call is most likely required for a submodule to work properly
+        super().__init__()
+
+    @property
+    def navx(self) -> navx.AHRS:
+        return self.navx
+    def execute(self):
+        self.navx.reset()
+
