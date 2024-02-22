83ae9431591cd24d0131f67721b969085d4a9397
KiraA-R <kira.romanowski@gmail.com>
2024-02-21 18:50:11 -0800

    generic selector widget

--- telemetry/__init__.py
+++ telemetry/__init__.py
@@ -2,3 +2,18 @@ from .swerve_telemetry import SwerveTelemetry
 # from .pid_telemetry import PIDTelemetry
 from . import chassis_heading_telemetry
 from .chassis_heading_telemetry import ChassisHeadingTelemetry
+
+
+def create_selector(sd_path: str, autos : list[str]) -> wpilib.SendableChooser:
+    """Creates a widget in smart dashboard that can select which test group to run from a list
+    :param sd_path: The path in smart dashboard to write the selected test group to
+    """
+
+    chooser = wpilib.SendableChooser()
+    for i, path in enumerate(autos):
+        chooser.addOption(path, i)  # Write the index to the dashboard, we'll use this to lookup which test to run
+
+    path = autos[-1]  # The last test in the test group is the default
+    chooser.setDefaultOption(path, len(autos) - 1)
+    wpilib.SmartDashboard.putData(sd_path, chooser)
+    return chooser
