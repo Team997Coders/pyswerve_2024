0213810288ee1384ef967cdaf117fa7dc739a7e7
James Anderson <james.r.anderson@utah.edu>
2024-02-23 13:53:12 -0800

    Figured out how to use PIDController widget.  Didn't work on first try because a @property field was missing

--- telemetry/chassis_heading_telemetry.py
+++ telemetry/chassis_heading_telemetry.py
@@ -4,7 +4,6 @@ from wpilib import SmartDashboard as sd
 import wpilib
 from typing import List, Tuple
 import math
-from telemetry import PIDEntry
 
 
 
@@ -12,15 +11,13 @@ class ChassisHeadingTelemetry:
     """Report swerve telemetry to the dashboard.  Compatible with WebComponents"""
 
     ChassisHeading = ChassisHeadingControl
-    pid_entry: PIDEntry
 
     def __init__(self, chassis_heading_control: ChassisHeadingControl) -> None:
         self.chassis_heading_control = chassis_heading_control
-        self.pid_entry = PIDEntry("Chassis Heading PID", chassis_heading_control.pid)
 
     def report_to_dashboard(self):
         """Write all module info to net tables"""
-        self.pid_entry.periodic()
+        sd.putData("CH: PID", self.chassis_heading_control.pid)
         sd.putNumber("CH: measured rotation", math.degrees(self.chassis_heading_control.getMeasurement()))
         sd.putNumber("CH: target angle for the target tracker", math.degrees(self.chassis_heading_control.target))
         sd.putNumber("CH: feedforward component", self.chassis_heading_control._feedforward_component)
@@ -31,17 +28,14 @@ class AxisPositionTelemetry:
 
     name: str
     axis_heading = AxisPositionControl
-    pid_entry: PIDEntry
 
     def __init__(self, name: str, axis_heading_control: AxisPositionControl) -> None:
         self.name = name
         self.axis_heading = axis_heading_control
-        self.pid_entry = PIDEntry(f"{name} position PID", axis_heading_control.pid)
 
     def report_to_dashboard(self):
         """Write all module info to net tables"""
-        self.pid_entry.periodic()
-
+        sd.putData(f"{self.name}: position PID", self.axis_heading.pid)
         sd.putNumber(f"{self.name}: measured position",  self.axis_heading.getMeasurement())
         sd.putNumber(f"{self.name}: target position", self.axis_heading.target)
         sd.putNumber(f"{self.name}: feedforward component", self.axis_heading._feedforward_component)
