a103500b5ad76649b3da6e33e11b2b7f588c96fb
Crondar <122586088+Crondar@users.noreply.github.com>
2024-02-23 19:01:09 -0800

    Should stop the mechanisms from retaining the state they were in when robot disabled.

--- commands/__init__.py
+++ commands/__init__.py
@@ -5,14 +5,14 @@ from . import index
 from .index import IndexOnIntake, IndexOnShoot, IndexOff
 
 from . import intake
-from .intake import Load, Outtake
+from .intake import Load, Outtake, IntakeOff
 # from . import reset_gyro
 # from .reset_gyro import ResetGyro
 
 from . import climb
 from . import drive
 from . import apriltags
-from .shoot import Shoot, SpinupShooter
+from .shoot import Shoot, SpinupShooter, SpindownShooter
 from .climb import ClimberFollow
 from .drive import Drive, TwinstickHeadingSetter
 from .apriltags import AprilTagPointer
