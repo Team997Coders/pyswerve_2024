aac7c8afb0ac4c2db037a832ab4b2154bb2ca479
Crondar <122586088+Crondar@users.noreply.github.com>
2024-03-05 12:16:18 -0800

    updated mechanism tests to include the climber.

--- commands/test_mechanisms.py
+++ commands/test_mechanisms.py
@@ -7,33 +7,44 @@ class TestMechanisms(commands2.Command):
     intake: subsystems.Intake
     shooter: subsystems.Shooter
     indexer: subsystems.Indexer
-    # climber: subsystems.Climber
+    climber: subsystems.Climber
     _command: commands2.Command
 
-    def __init__(self, intake: subsystems.Intake, shooter: subsystems.Shooter, indexer: subsystems.Indexer):
+    def __init__(self, intake: subsystems.Intake, shooter: subsystems.Shooter, indexer: subsystems.Indexer,
+                 climber: subsystems.Climber):
         super().__init__()
+        test_time = 1.0
         self._intake = intake
         self._shooter = shooter
         self._indexer = indexer
-        # self._climber = climber
+        self._climber = climber
         self._command = commands2.SequentialCommandGroup(
             commands.IntakeOn(self._intake),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.IntakeOff(self._intake),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.SpinupShooter(self._shooter),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.SpindownShooter(self._shooter),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.IndexOnIntake(self._indexer),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.IndexOff(self._indexer),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.Outtake(self._intake, self._indexer),
-            commands2.WaitCommand(.5),
+            commands2.WaitCommand(test_time),
             commands.IndexOff(self._indexer),
+            commands2.WaitCommand(test_time),
             commands.IntakeOff(self._intake),
-            commands2.WaitCommand(.5)
+            commands2.WaitCommand(test_time),
+            commands.ClimberUp(self._climber),
+            commands2.WaitCommand(test_time / 2.0),
+            commands.ClimberStop(self._climber),
+            commands2.WaitCommand(test_time / 2.0),
+            commands.ClimberDown(self._climber),
+            commands2.WaitCommand(test_time / 2.0),
+            commands.ClimberStop(self._climber)
+
         )
 
     def execute(self):
