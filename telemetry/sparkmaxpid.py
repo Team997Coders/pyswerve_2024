8deac332ee059ff8b325ca08ce42b8e1f967fe33
Alex_He <118498365+Yuxuan1103@users.noreply.github.com>
2024-03-20 20:08:56 -0700

    robot centric

--- telemetry/sparkmaxpid.py
+++ telemetry/sparkmaxpid.py
@@ -6,13 +6,13 @@ class SparkMaxPIDEntry(ntcore.NTSendable):
     _pid: rev.SparkMaxPIDController
     _setpoint: float
 
-    def initSendable(self, builder: ntcore.NTSendableBuilder) -> None:
-        builder.setSmartDashboardType("PIDController")
-        builder.addDoubleProperty("p", lambda: self._pid.getP(), lambda x: self._pid.setP(x))
-        builder.addDoubleProperty("i", lambda: self._pid.getI(), lambda x: self._pid.setI(x))
-        builder.addDoubleProperty("d", lambda: self._pid.getD(), lambda x: self._pid.setD(x))
-        builder.addDoubleProperty("izone", lambda: self._pid.getIZone(), lambda x: self._pid.setIZone(x))
-        builder.addDoubleProperty("setpoint", getter=lambda: self.setpoint, setter=lambda x: self.set_setpoint(x))
+    # def initSendable(self, builder: ntcore.NTSendableBuilder) -> None:
+    #     builder.setSmartDashboardType("PIDController")
+    #     builder.addDoubleProperty("p", lambda: self._pid.getP(), lambda x: self._pid.setP(x))
+    #     builder.addDoubleProperty("i", lambda: self._pid.getI(), lambda x: self._pid.setI(x))
+    #     builder.addDoubleProperty("d", lambda: self._pid.getD(), lambda x: self._pid.setD(x))
+    #     builder.addDoubleProperty("izone", lambda: self._pid.getIZone(), lambda x: self._pid.setIZone(x))
+    #     builder.addDoubleProperty("setpoint", getter=lambda: self.setpoint, setter=lambda x: self.set_setpoint(x))
 
     @property
     def setpoint(self) -> float:
